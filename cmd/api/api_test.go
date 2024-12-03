ac5bbc55fcf401e7d1b6159e71977e7c80f5d1d0
Immortal Pony <mannulus.immortalis@gmail.com>
2024-12-03 12:42:28 +0300

    added more tests

--- cmd/api/api_test.go
+++ cmd/api/api_test.go
@@ -3,12 +3,14 @@ package main
 import (
 	"bytes"
 	"context"
+	"database/sql"
 	"github.com/rs/zerolog"
 	"io"
 	"net/http"
 	"os"
 	"regexp"
 	"testing"
+	"time"
 
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/google/uuid"
@@ -61,6 +63,7 @@ func TestCreateItem(t *testing.T) {
 		// start server
 		api := api.New(&log, dbConn, jwtAuth, kafkaMock)
 		go func() { _ = api.Run(":9081") }()
+		time.Sleep(10 * time.Millisecond)
 		defer api.Close()
 
 		// make request
@@ -107,6 +110,7 @@ func TestCreateItem(t *testing.T) {
 		// start server
 		api := api.New(&log, dbConn, jwtAuth, kafkaMock)
 		go func() { _ = api.Run(":9082") }()
+		time.Sleep(10 * time.Millisecond)
 		defer api.Close()
 
 		// make request
@@ -146,6 +150,7 @@ func TestCreateItem(t *testing.T) {
 		// start server
 		api := api.New(&log, dbConn, jwtAuth, kafkaMock)
 		go func() { _ = api.Run(":9083") }()
+		time.Sleep(10 * time.Millisecond)
 		defer api.Close()
 
 		// make request
@@ -185,6 +190,7 @@ func TestCreateItem(t *testing.T) {
 		// start server
 		api := api.New(&log, dbConn, jwtAuth, kafkaMock)
 		go func() { _ = api.Run(":9084") }()
+		time.Sleep(10 * time.Millisecond)
 		defer api.Close()
 
 		// make request
@@ -204,4 +210,345 @@ func TestCreateItem(t *testing.T) {
 		assert.Equal(t, http.StatusForbidden, resp.StatusCode)
 		assert.Equal(t, `{"error":"Access denied"}`, string(respBody))
 	})
+}
+
+func TestUpdateItem(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		ctx := context.Background()
+		log := zerolog.New(os.Stdout).With().Timestamp().Logger()
+		id := uuid.MustParse("3f00e7f6-f9c8-4a3a-8a27-2bd529b161e4")
+
+		// mock db
+		conn, mock, err := sqlmock.New()
+		assert.NoError(t, err)
+		defer func() {
+			_ = conn.Close()
+		}()
+		dbConn := db.NewFromConn(conn)
+		mock.ExpectExec(regexp.QuoteMeta(`UPDATE companies 
+	SET
+		name=COALESCE($2, name), 
+		description=COALESCE($3, description), 
+		employee_count=COALESCE($4, employee_count), 
+		is_registered=COALESCE($5, is_registered), 
+		legal_type=COALESCE($6, legal_type)
+	WHERE id = $1`)).
+			WithArgs(id, nil, nil, 1, nil, "Sole Proprietorship").WillReturnResult(sqlmock.NewResult(0, 1))
+
+		// mock kafka
+		kafkaMock := mocks.NewNotifyInt(t)
+		kafkaMock.On("Send", models.EventNotifications{ID: id, Event: models.EventTypeUpdated}).Return(nil)
+
+		// real jwt
+		jwtAuth, err := auth.New(jwtKey)
+		assert.NoError(t, err)
+		token, err := jwtAuth.Generate([]string{"reader", "writer"})
+		assert.NoError(t, err)
+
+		// start server
+		api := api.New(&log, dbConn, jwtAuth, kafkaMock)
+		go func() { _ = api.Run(":9081") }()
+		time.Sleep(10 * time.Millisecond)
+		defer api.Close()
+
+		// make request
+		reqBody := []byte(`{"employee_count":1, "type":"Sole Proprietorship"}`)
+		req, err := http.NewRequestWithContext(ctx, http.MethodPatch, "http://localhost:9081/api/v1/company/"+id.String(), bytes.NewBuffer(reqBody))
+		assert.NoError(t, err)
+		req.Header.Add("Authorization", "Bearer "+token)
+		resp, err := http.DefaultClient.Do(req)
+		assert.NoError(t, err)
+		_ = resp.Body.Close()
+
+		// test result
+		assert.Equal(t, http.StatusOK, resp.StatusCode)
+	})
+
+	t.Run("error_duplicate", func(t *testing.T) {
+		ctx := context.Background()
+		log := zerolog.New(os.Stdout).With().Timestamp().Logger()
+		id := uuid.MustParse("3f00e7f6-f9c8-4a3a-8a27-2bd529b161e4")
+
+		// mock db
+		conn, mock, err := sqlmock.New()
+		assert.NoError(t, err)
+		defer func() {
+			_ = conn.Close()
+		}()
+		dbConn := db.NewFromConn(conn)
+		mock.ExpectExec(regexp.QuoteMeta(`UPDATE companies 
+	SET
+		name=COALESCE($2, name), 
+		description=COALESCE($3, description), 
+		employee_count=COALESCE($4, employee_count), 
+		is_registered=COALESCE($5, is_registered), 
+		legal_type=COALESCE($6, legal_type)
+	WHERE id = $1`)).
+			WithArgs(id, nil, nil, 1, nil, "Sole Proprietorship").WillReturnError(errDuplicate)
+
+		// mock kafka
+		kafkaMock := mocks.NewNotifyInt(t)
+
+		// real jwt
+		jwtAuth, err := auth.New(jwtKey)
+		assert.NoError(t, err)
+		token, err := jwtAuth.Generate([]string{"reader", "writer"})
+		assert.NoError(t, err)
+
+		// start server
+		api := api.New(&log, dbConn, jwtAuth, kafkaMock)
+		go func() { _ = api.Run(":9082") }()
+		time.Sleep(10 * time.Millisecond)
+		defer api.Close()
+
+		// make request
+		reqBody := []byte(`{"employee_count":1, "type":"Sole Proprietorship"}`)
+		req, err := http.NewRequestWithContext(ctx, http.MethodPatch, "http://localhost:9082/api/v1/company/"+id.String(), bytes.NewBuffer(reqBody))
+		assert.NoError(t, err)
+		req.Header.Add("Authorization", "Bearer "+token)
+		resp, err := http.DefaultClient.Do(req)
+		assert.NoError(t, err)
+		defer func() {
+			_ = resp.Body.Close()
+		}()
+		respBody, err := io.ReadAll(resp.Body)
+		assert.NoError(t, err)
+
+		// test result
+		assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
+		assert.Equal(t, `{"error":"Duplicate item name"}`, string(respBody))
+	})
+
+	t.Run("error_validation", func(t *testing.T) {
+		ctx := context.Background()
+		log := zerolog.New(os.Stdout).With().Timestamp().Logger()
+		id := uuid.MustParse("3f00e7f6-f9c8-4a3a-8a27-2bd529b161e4")
+
+		// mock db
+		dbConn := mocks.NewStorageInt(t)
+
+		// mock kafka
+		kafkaMock := mocks.NewNotifyInt(t)
+
+		// real jwt
+		jwtAuth, err := auth.New(jwtKey)
+		assert.NoError(t, err)
+		token, err := jwtAuth.Generate([]string{"reader", "writer"})
+		assert.NoError(t, err)
+
+		// start server
+		api := api.New(&log, dbConn, jwtAuth, kafkaMock)
+		go func() { _ = api.Run(":9083") }()
+		time.Sleep(10 * time.Millisecond)
+		defer api.Close()
+
+		// make request
+		reqBody := []byte(`{"type":"LTD"}`)
+		req, err := http.NewRequestWithContext(ctx, http.MethodPatch, "http://localhost:9083/api/v1/company/"+id.String(), bytes.NewBuffer(reqBody))
+		assert.NoError(t, err)
+		req.Header.Add("Authorization", "Bearer "+token)
+		resp, err := http.DefaultClient.Do(req)
+		assert.NoError(t, err)
+		defer func() {
+			_ = resp.Body.Close()
+		}()
+		respBody, err := io.ReadAll(resp.Body)
+		assert.NoError(t, err)
+
+		// test result
+		assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
+		assert.Equal(t, `{"error":"Invalid type"}`, string(respBody))
+	})
+}
+
+func TestDeleteItem(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		ctx := context.Background()
+		log := zerolog.New(os.Stdout).With().Timestamp().Logger()
+		id := uuid.MustParse("3f00e7f6-f9c8-4a3a-8a27-2bd529b161e4")
+
+		// mock db
+		conn, mock, err := sqlmock.New()
+		assert.NoError(t, err)
+		defer func() {
+			_ = conn.Close()
+		}()
+		dbConn := db.NewFromConn(conn)
+		mock.ExpectExec(regexp.QuoteMeta(`DELETE FROM companies WHERE id = $1`)).
+			WithArgs(id).WillReturnResult(sqlmock.NewResult(0, 1))
+
+		// mock kafka
+		kafkaMock := mocks.NewNotifyInt(t)
+		kafkaMock.On("Send", models.EventNotifications{ID: id, Event: models.EventTypeDeleted}).Return(nil)
+
+		// real jwt
+		jwtAuth, err := auth.New(jwtKey)
+		assert.NoError(t, err)
+		token, err := jwtAuth.Generate([]string{"reader", "writer"})
+		assert.NoError(t, err)
+
+		// start server
+		api := api.New(&log, dbConn, jwtAuth, kafkaMock)
+		go func() { _ = api.Run(":9081") }()
+		time.Sleep(10 * time.Millisecond)
+		defer api.Close()
+
+		// make request
+		req, err := http.NewRequestWithContext(ctx, http.MethodDelete, "http://localhost:9081/api/v1/company/"+id.String(), http.NoBody)
+		assert.NoError(t, err)
+		req.Header.Add("Authorization", "Bearer "+token)
+		resp, err := http.DefaultClient.Do(req)
+		assert.NoError(t, err)
+		_ = resp.Body.Close()
+
+		// test result
+		assert.Equal(t, http.StatusOK, resp.StatusCode)
+	})
+
+	t.Run("error_not_found", func(t *testing.T) {
+		ctx := context.Background()
+		log := zerolog.New(os.Stdout).With().Timestamp().Logger()
+		id := uuid.MustParse("3f00e7f6-f9c8-4a3a-8a27-2bd529b161e4")
+
+		// mock db
+		conn, mock, err := sqlmock.New()
+		assert.NoError(t, err)
+		defer func() {
+			_ = conn.Close()
+		}()
+		dbConn := db.NewFromConn(conn)
+		mock.ExpectExec(regexp.QuoteMeta(`DELETE FROM companies WHERE id = $1`)).
+			WithArgs(id).WillReturnResult(sqlmock.NewResult(0, 1)).WillReturnResult(sqlmock.NewResult(0, 0))
+
+		// mock kafka
+		kafkaMock := mocks.NewNotifyInt(t)
+
+		// real jwt
+		jwtAuth, err := auth.New(jwtKey)
+		assert.NoError(t, err)
+		token, err := jwtAuth.Generate([]string{"reader", "writer"})
+		assert.NoError(t, err)
+
+		// start server
+		api := api.New(&log, dbConn, jwtAuth, kafkaMock)
+		go func() { _ = api.Run(":9082") }()
+		time.Sleep(10 * time.Millisecond)
+		defer api.Close()
+
+		// make request
+		req, err := http.NewRequestWithContext(ctx, http.MethodDelete, "http://localhost:9082/api/v1/company/"+id.String(), http.NoBody)
+		assert.NoError(t, err)
+		req.Header.Add("Authorization", "Bearer "+token)
+		resp, err := http.DefaultClient.Do(req)
+		assert.NoError(t, err)
+		defer func() {
+			_ = resp.Body.Close()
+		}()
+		respBody, err := io.ReadAll(resp.Body)
+		assert.NoError(t, err)
+
+		// test result
+		assert.Equal(t, http.StatusNotFound, resp.StatusCode)
+		assert.Equal(t, `{"error":"Item not found"}`, string(respBody))
+	})
+
+}
+
+func TestGetItem(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		ctx := context.Background()
+		log := zerolog.New(os.Stdout).With().Timestamp().Logger()
+		id := uuid.MustParse("3f00e7f6-f9c8-4a3a-8a27-2bd529b161e4")
+
+		// mock db
+		conn, mock, err := sqlmock.New()
+		assert.NoError(t, err)
+		defer func() {
+			_ = conn.Close()
+		}()
+		dbConn := db.NewFromConn(conn)
+		rows := sqlmock.NewRows([]string{"id", "name", "description", "employee_count", "is_registered", "legal_type"})
+		rows.AddRow(id.String(), "name", "description", 3, true, "Corporations")
+		mock.ExpectQuery(regexp.QuoteMeta(`SELECT id, name, description, employee_count, is_registered, legal_type FROM companies WHERE id = $1`)).
+			WithArgs(id).WillReturnRows(rows)
+
+		// mock kafka
+		kafkaMock := mocks.NewNotifyInt(t)
+
+		// real jwt
+		jwtAuth, err := auth.New(jwtKey)
+		assert.NoError(t, err)
+		token, err := jwtAuth.Generate([]string{"reader", "writer"})
+		assert.NoError(t, err)
+
+		// start server
+		api := api.New(&log, dbConn, jwtAuth, kafkaMock)
+		go func() { _ = api.Run(":9081") }()
+		time.Sleep(10 * time.Millisecond)
+		defer api.Close()
+
+		// make request
+		req, err := http.NewRequestWithContext(ctx, http.MethodGet, "http://localhost:9081/api/v1/company/"+id.String(), http.NoBody)
+		assert.NoError(t, err)
+		req.Header.Add("Authorization", "Bearer "+token)
+		resp, err := http.DefaultClient.Do(req)
+		assert.NoError(t, err)
+		defer func() {
+			_ = resp.Body.Close()
+		}()
+		respBody, err := io.ReadAll(resp.Body)
+		assert.NoError(t, err)
+
+		// test result
+		assert.Equal(t, http.StatusOK, resp.StatusCode)
+		assert.Equal(t, `{"id":"`+id.String()+`","name":"name","description":"description","employee_count":3,"is_registered":true,"type":"Corporations"}`, string(respBody))
+	})
+
+	t.Run("error_not_found", func(t *testing.T) {
+		ctx := context.Background()
+		log := zerolog.New(os.Stdout).With().Timestamp().Logger()
+		id := uuid.MustParse("3f00e7f6-f9c8-4a3a-8a27-2bd529b161e4")
+
+		// mock db
+		conn, mock, err := sqlmock.New()
+		assert.NoError(t, err)
+		defer func() {
+			_ = conn.Close()
+		}()
+		dbConn := db.NewFromConn(conn)
+		mock.ExpectQuery(regexp.QuoteMeta(`SELECT id, name, description, employee_count, is_registered, legal_type FROM companies WHERE id = $1`)).
+			WithArgs(id).WillReturnError(sql.ErrNoRows)
+
+		// mock kafka
+		kafkaMock := mocks.NewNotifyInt(t)
+
+		// real jwt
+		jwtAuth, err := auth.New(jwtKey)
+		assert.NoError(t, err)
+		token, err := jwtAuth.Generate([]string{"reader", "writer"})
+		assert.NoError(t, err)
+
+		// start server
+		api := api.New(&log, dbConn, jwtAuth, kafkaMock)
+		go func() { _ = api.Run(":9082") }()
+		time.Sleep(10 * time.Millisecond)
+		defer api.Close()
+
+		// make request
+		req, err := http.NewRequestWithContext(ctx, http.MethodGet, "http://localhost:9082/api/v1/company/"+id.String(), http.NoBody)
+		assert.NoError(t, err)
+		req.Header.Add("Authorization", "Bearer "+token)
+		resp, err := http.DefaultClient.Do(req)
+		assert.NoError(t, err)
+		defer func() {
+			_ = resp.Body.Close()
+		}()
+		respBody, err := io.ReadAll(resp.Body)
+		assert.NoError(t, err)
+
+		// test result
+		assert.Equal(t, http.StatusNotFound, resp.StatusCode)
+		assert.Equal(t, `{"error":"Item not found"}`, string(respBody))
+	})
+
 }
--- go.mod
+++ go.mod
@@ -5,7 +5,6 @@ go 1.22.2
 require (
 	github.com/DATA-DOG/go-sqlmock v1.5.2
 	github.com/IBM/sarama v1.43.3
-	github.com/brianvoe/gofakeit/v6 v6.28.0
 	github.com/gin-contrib/cors v1.7.2
 	github.com/gin-gonic/gin v1.10.0
 	github.com/golang-jwt/jwt/v5 v5.2.1
